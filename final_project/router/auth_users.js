49a998e7ff084278707c87af4fe888858a22e666
Ansh Gupta <96367843+Kami-wasTaken@users.noreply.github.com>
2023-06-27 15:17:42 +0530

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,17 +5,49 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username)=>{ 
+    let userwithsamename = users.filter((user)=>{
+    return user[0] === username});
+    if(userwithsamename.length > 0){
+        return(false)
+    }
+    else{
+        return(true)
+    }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{ 
+    if(username && password){
+        userForLogin = users.filter((user)=>{
+            return user[0] === username && user[1] === password
+        });
+        if(userForLogin.length > 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
+    const username = req.body.username
+    const password = req.body.password
+  if (!username || !password){
+      return res.status(404).json({message: "Error logging in"});
+  }
+  if(authenticatedUser(username, password)){
+      let accessToken = jwt.sign({
+          data: password
+      }, 'access', {expiresIn: 60 * 60});
+
+      req.session.authorization = {
+          accessToken, username
+      }
+      return res.status(200).send("User successfully logged in");
+  }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
